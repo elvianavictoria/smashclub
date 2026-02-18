387378a4e75a584ecf9f88f5eb634064682f0be7
deceeeeee <micha3l_dct@yahoo.com>
2026-02-18 15:02:58 +0700

    Add backend and FE url

--- src/main/java/com/backendsyndicate/smashclub/admin/dto/response/RespAdminPlayerListDTO.java
+++ src/main/java/com/backendsyndicate/smashclub/admin/dto/response/RespAdminPlayerListDTO.java
@@ -11,7 +11,7 @@ public class RespAdminPlayerListDTO {
     private String id;
     private String fullName;
     private String email;
-    private int status;
+    private byte status;
     private String createdAt;
     private String lockedUntil;
 }
