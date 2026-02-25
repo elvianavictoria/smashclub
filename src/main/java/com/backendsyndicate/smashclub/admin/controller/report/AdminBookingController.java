98a5322c9ed5c7a58a3acf4d41c623d3370dc6d4
deceeeeee <micha3l_dct@yahoo.com>
2026-02-25 11:56:10 +0700

    Add new endpoint for CMS

--- src/main/java/com/backendsyndicate/smashclub/admin/controller/report/AdminBookingController.java
+++ src/main/java/com/backendsyndicate/smashclub/admin/controller/report/AdminBookingController.java
@@ -1,11 +1,13 @@
 package com.backendsyndicate.smashclub.admin.controller.report;
 
 import com.backendsyndicate.smashclub.admin.service.report.AdminBookingService;
+import com.backendsyndicate.smashclub.common.constant.PermissionConstant;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,13 +16,13 @@ public class AdminBookingController {
     @Autowired
     private AdminBookingService adminBookingService;
 
-//    @PreAuthorize("hasAuthority('" + PermissionConstant.BOOKING_READ_CODE + "')")
+    @PreAuthorize("hasAuthority('" + PermissionConstant.BOOKING_SALES_READ_CODE + "')")
     @GetMapping
     public ResponseEntity<Object> bookingStatistic(@RequestParam int year, HttpServletRequest request) {
         return adminBookingService.statistic(year, request);
     }
 
-//    @PreAuthorize("hasAuthority('" + PermissionConstant.BOOKING_READ_CODE + "')")
+    @PreAuthorize("hasAuthority('" + PermissionConstant.BOOKING_SALES_READ_CODE + "')")
     @GetMapping("list")
     public ResponseEntity<Object> bookingDaily(
             @RequestParam int year,
@@ -34,13 +36,13 @@ public class AdminBookingController {
         return adminBookingService.list(year, month, keyword, pageable, request);
     }
 
-//    @PreAuthorize("hasAuthority('" + PermissionConstant.BOOKING_DETAIL_CODE + "')")
+    @PreAuthorize("hasAuthority('" + PermissionConstant.BOOKING_SALES_DETAIL_CODE + "')")
     @GetMapping("detail/{bookingCode}")
     public ResponseEntity<Object> bookingDetail(@PathVariable String bookingCode, HttpServletRequest request) {
         return adminBookingService.detail(bookingCode, request);
     }
 
-    //    @PreAuthorize("hasAuthority('" + PermissionConstant.BOOKING_PROCESS_CODE + "')")
+    @PreAuthorize("hasAuthority('" + PermissionConstant.BOOKING_SALES_PROCESS_CODE + "')")
     @PostMapping("process/{bookingCode}")
     public ResponseEntity<Object> bookingProcess(@PathVariable String bookingCode, @RequestBody int status, HttpServletRequest request) {
         return adminBookingService.process(bookingCode, status, request);
